f3fb75b808df4b911e3c356e1fe002723b371f80
Vishakh Sandwar <vishakhs74@gmail.com>
2024-03-31 00:42:10 -0400

    address to zip code

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ import numpy as np
 # from flask_cors import CORS
 from util.noaa import get_forecasts
 from util.evap import sprinkle
+import requests
 
 app = Flask(__name__)
 
@@ -11,8 +12,8 @@ TO-DO:
 Get target & R_n
 '''
 
-@app.route('/<zip_code>', methods=['GET'])
-def hello(zip_code):
+@app.route('/zip/<zip_code>', methods=['GET'])
+def sprinkle(zip_code):
     res = get_forecasts(zip_code)
     temp = res['temperature']
     wind = res['windSpeed']
@@ -21,3 +22,28 @@ def hello(zip_code):
     target = np.random.randint(0, 10) # TODO
     output = sprinkle(target, prec, temp, wind, dew, y=0.066, Rn=20, G=0)
     return output
+
+@app.route('/address/<address>', methods=['GET'])
+def get_location(address):
+    keys = address.split(',')
+    print(keys)
+    # Define the API endpoint URL
+    url = 'https://nominatim.openstreetmap.org/search?q=' + '+'.join(keys) + '&format=json&polygon_kml=1&addressdetails=1'
+    # Make a GET request to the API endpoint
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+
+    d = dict()
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        d['lat'] = response.json()[0]['lat']
+        d['long'] = response.json()[0]['lon']
+        d['zip_code'] = response.json()[0]['address']['postcode']
+        return d
+
+    else:
+        # Print an error message if the request was unsuccessful
+        print('Error:', response.status_code)
+        return 'Error:', response.status_code
